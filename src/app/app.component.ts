d3ed47e9b0e8a0e2cc78927d7fa7c0bbdce6c9c2
Amanda M Namikata <amandamnamikata@gmail.com>
2021-11-18 17:32:41 -0300

    Comentarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shitapp';
+  data=[
+    {
+      id:'1',
+      name:'Amanda Midori Namikata',
+      ru:'3414594',
+      date:'17/06/19999',
+      curso:'Análise e Desenvolvimento de Sistemas'
+    },
+    {
+      id:'2',
+      name:'Devil',
+      ru:'6666666',
+      date:'99/99/99999',
+      curso:'Análise e Desenvolvimento de Sistemas'
+    },
+    {
+      id:'3',
+      name:'Mark',
+      ru:'6666666',
+      date:'99/99/99999',
+      curso:'Análise e Desenvolvimento de Sistemas'
+    },
+    {
+      id:'4',
+      name:'Bady',
+      ru:'6666666',
+      date:'99/99/99999',
+      curso:'Análise e Desenvolvimento de Sistemas'
+    },
+    {
+      id:'5',
+      name:'Jeezus',
+      ru:'6666666',
+      date:'99/99/99999',
+      curso:'Análise e Desenvolvimento de Sistemas'
+    },
+
+  ]
 }
