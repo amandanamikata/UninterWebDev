e10d19c385219f317c96ee17719c3b29fccd8503
Amanda M Namikata <amandamnamikata@gmail.com>
2021-11-20 22:19:17 -0300

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shitapp';
+  title = 'trabalho uninter';
   data=[
     {
       id:'1',
